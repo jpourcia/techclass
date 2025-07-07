8594fc70baf2722b884ddd1e699a66f34894db80
JC Pourciaux <jc.pourciaux@hotmail.com>
2025-07-07 18:37:26 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,34 @@
+const questionData = {
+  "T0": [
+    {
+      "question": "What is a good safety precaution when using a generator?",
+      "answers": [
+        "Use indoors",
+        "Connect neutral to ground",
+        "Use in well-ventilated area",
+        "Never use with inverter"
+      ],
+      "correct": "C"
+    }
+  ],
+  "T1": [
+    {
+      "question": "What is an FCC-issued call sign?",
+      "answers": [
+        "A code used to access repeaters",
+        "A unique designator assigned to a licensed operator",
+        "A type of transmission mode",
+        "A frequency allocation chart"
+      ],
+      "correct": "B"
+    }
+  ],
+  "T2": [],
+  "T3": [],
+  "T4": [],
+  "T5": [],
+  "T6": [],
+  "T7": [],
+  "T8": [],
+  "T9": []
+};
